def7723d5bff9348e3e91f3c454ea6c875d484c6
kllilizxc <541796726@qq.com>
2017-04-30 06:47:21 +0800

    init

---
+++ src/components/SignIn.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="sign-in-page">
+		<div class="row">
+		<div class="input-field col s6">
+			<input id="username" type="text" class="validate"></input>
+			<label for="username">user name</label>
+		</div>
+	</div>
+	<div class="row">
+		<div class="input-field col s6">
+			<input id="password" type="text" class="validate"></input>
+			<label for="password">password</label>
+		</div>
+	</div>
+	</div>
+</template>
+
+<script>
+	import 'materialize'
+
+	export default {
+	}
+</script>
+
+<style scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import SignIn from '@/components/SignIn'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/SignIn',
+      name: 'SignIn',
+      component: SignIn
+    }
+  ]
+})
