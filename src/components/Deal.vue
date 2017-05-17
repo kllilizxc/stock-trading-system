8278be631055b428021cee39776127cb4e63cbb1
kllilizxc <541796726@qq.com>
2017-05-18 00:44:32 +0800

    write the style of deal page

--- src/components/Deal.vue
+++ src/components/Deal.vue
@@ -1,7 +1,54 @@
 <template>
-  <span>Deal Page</span>
+  <md-card id="deal-card">
+    <span class="title md-title md-primary">交易</span>
+    <form novalidate id="deal-form">
+      <md-input-container>
+        <label for="type">交易类型</label>
+        <md-select name="type" required>
+          <md-option value="0">限价申报</md-option>
+          <md-option value="1">撤销申报</md-option>
+          <md-option value="2">转限申报</md-option>
+        </md-select>
+      </md-input-container>
+      <md-input-container>
+        <label for="action">买/卖</label>
+        <md-select name="action" required>
+          <md-option value="0">买</md-option>
+          <md-option value="1">卖</md-option>
+          <md-option value="2">买/卖</md-option>
+        </md-select>
+      </md-input-container>
+      <md-input-container>
+        <label for="count">数量</label>
+        <md-input type="number" name="count" required></md-input>
+      </md-input-container>
+      <md-input-container>
+        <label for="price">价格</label>
+        <md-input type="number" name="price"></md-input>
+      </md-input-container>
+      <md-input-container>
+        <label for="user-id">用户 ID</label>
+        <md-input name="user-id" required></md-input>
+      </md-input-container>
+      <md-input-container>
+        <label for="stock-id">股票 ID</label>
+        <md-input name="stock-id" required></md-input>
+      </md-input-container>
+      <md-button class="md-primary md-raised">提交</md-button>
+    </form>
+  </md-card>
 </template>
 
 <script></script>
 
-<style scoped></style>
+<style scoped>
+  .title {
+    margin-bottom: 16px;
+  }
+  #deal-card {
+    max-width: 400px;
+    width: 100%;
+    margin: auto;
+    padding: 32px;
+  }
+</style>
